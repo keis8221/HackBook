bd3c1b969ab2f6e6fe77487caa0ccec8863c0040
keis8221 <kei_s8221@icloud.com>
2023-05-12 01:06:39 +0900

    setting: setting to connect DB

--- api/main.go
+++ api/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/keis8221/surprise/api/db"
+)
 
 func main() {
 	r := gin.Default()
@@ -10,4 +13,6 @@ func main() {
 			})
 	})
 	r.Run()
+	
+	db.Init()
 }
